cf0bc48c12ba5055bf136ad5b46aa891922571a8
Administrator <Administrator@10.250.1.19>
2022-03-10 22:43:07 -0500

    runner updates

---
+++ src/test/java/Runner/TestRunners.java
@@ -0,0 +1,21 @@
+package Runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.*;
+
+    @RunWith(Cucumber.class)
+	@CucumberOptions(
+	        features = "./src/test/resources/features",
+	        glue={"stepDefinitions"},
+	        dryRun =false,
+	        monochrome = true,
+	        tags = {"@inboundpageNumberValidation"},
+	        plugin = { "pretty", "html:target/cucumber-html-report", "json:target/cucumber/cucumber.json" ,"junit:target/cucxml/cucumber.xml"}
+	)
+	public class TestRunners {
+
+	
+}
+
+
