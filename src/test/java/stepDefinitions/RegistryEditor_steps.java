234dc4d0fd8ce2370756545f4b068d5b234c56c8
abbas <abbas@softlinx.com>
2022-03-16 10:01:11 -0400

    update feature file

--- src/test/java/stepDefinitions/RegistryEditor_steps.java
+++ src/test/java/stepDefinitions/RegistryEditor_steps.java
@@ -7,7 +7,6 @@ import io.restassured.response.Response;
 import java.util.*;
 import java.io.*;
 
-
 public class RegistryEditor_steps {
 
 @Given("I want to write a step to change registryEditor")
@@ -19,10 +18,7 @@ public void i_want_to_write_a_step_to_change_registryEditor() throws IOException
 	Process newProcess = builder.start();
 	newProcess.waitFor();
 	System.out.println(newProcess);
-	
-	
-	// new updates///--//
-	
+		
 	
 //	String[] cmd ={"regedit.exe","C:/Users/abbas/Desktop/6.reg","C:\>cmdkey /generic:10.250.1.99:8080 /user:abbas /pass:Softlinx1!",
 //	"C:\>mstsc.exe /v:10.250.1.99"};
