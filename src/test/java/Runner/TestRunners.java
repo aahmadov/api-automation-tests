e06b9027098b298ec80b535b600d06962a02d2e3
abbas <abbas@softlinx.com>
2022-03-16 01:18:55 -0400

    latest commit

--- src/test/java/Runner/TestRunners.java
+++ src/test/java/Runner/TestRunners.java
@@ -10,7 +10,7 @@ import io.cucumber.junit.*;
 	        glue={"stepDefinitions"},
 	        dryRun =false,
 	        monochrome = true,
-	        tags = {"  @db_test"},
+	        tags = {"@regression"},
 	        plugin = { "pretty", "html:target/cucumber-html-report", "json:target/cucumber/cucumber.json" ,"junit:target/cucxml/cucumber.xml"}
 	)
 	public class TestRunners {
