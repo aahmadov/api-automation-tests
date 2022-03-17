cbeb3993d499410218cf7313f5de3f9832e28f6e
abbas <abbas@softlinx.com>
2022-03-17 00:28:59 -0400

    reg modifaction commit

--- src/test/java/stepDefinitions/RegistryEditor_steps.java
+++ src/test/java/stepDefinitions/RegistryEditor_steps.java
@@ -5,20 +5,26 @@ import java.io.IOException;
 import io.cucumber.java.en.*;
 import io.restassured.response.Response;
 import java.util.*;
+import java.awt.Desktop;
 import java.io.*;
+
 
 public class RegistryEditor_steps {
 
 @Given("I want to write a step to change registryEditor")
 public void i_want_to_write_a_step_to_change_registryEditor() throws IOException, InterruptedException  {
 	
-	ProcessBuilder builder = new ProcessBuilder(new String[] { "cmd", "/c","C:/>cmdkey /generic:10.250.1.99:8080 /user:abbas /pass:Softlinx1!",
-			"C:/>mstsc.exe /v:10.250.1.99","C:/Users/abbas/Desktop/10.reg"});
+	ProcessBuilder builder = new ProcessBuilder(new String[] {"cmd", "/c","regedit","/s","c:/Users/Administrator/Desktop/blank.reg"});
+	
+	//Desktop.getDesktop().open(new File("c:/Users/abbas/Desktop/10.reg"));
 	
 	Process newProcess = builder.start();
 	newProcess.waitFor();
 	System.out.println(newProcess);
-		
+	
+	
+
+	
 	
 //	String[] cmd ={"regedit.exe","C:/Users/abbas/Desktop/6.reg","C:\>cmdkey /generic:10.250.1.99:8080 /user:abbas /pass:Softlinx1!",
 //	"C:\>mstsc.exe /v:10.250.1.99"};
