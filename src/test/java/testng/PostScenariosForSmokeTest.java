fd3379ea3940333a833026d0b690087f55c84d7c
abbas <abbas@softlinx.com>
2022-09-15 09:49:57 -0400

    TestNG new class updated

--- src/test/java/testng/PostScenariosForSmokeTest.java
+++ src/test/java/testng/PostScenariosForSmokeTest.java
@@ -80,27 +80,5 @@ public class PostScenariosForSmokeTest extends TestBase {
         Assert.assertEquals(data.get("expectedErrorMessage"), actual);
     }
 
-    @Test(testName = "Send Fax Data with multiple recipient Details")
-    @Parameters({"postCallUrl", "fileName1", "fileName2", "recipientData1", "recipientData2", "expectedStatusCode"})
-    void sendFaxDataWithMultipleRecipientDetails(String postCallUrl, String fileName1,
-                                                 String fileName2,
-                                                 String recipientData1, String recipientData2,
-                                                 int expectedStatusCode) {
-        Response response = RestRequestUtils.createFaxmultipRecip(postCallUrl, FileReader.readfile(fileName1),
-                FileReader.readfile(fileName2), recipientData1, recipientData2);
-        System.out.println("------------------------------------------------------------------------");
-        System.out.println("**" + postCallUrl);
-        System.out.println("**" + FileReader.readfile(fileName1));
-        System.out.println("**" + FileReader.readfile(fileName2));
-        System.out.println("**" + recipientData1);
-        System.out.println("**" + recipientData2);
-        System.out.println("------------------------------------------------------------------------");
 
-        assertEquals(expectedStatusCode, response.getStatusCode());
-
-        String resp = response.prettyPrint();
-        String data = JsonPath.read(resp, "$.FaxInfo[0].FaxId").toString();
-
-        Assert.assertTrue(resp.contains(data));
-    }
 }
