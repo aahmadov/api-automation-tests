23a8245c4372d8df7a9a07ca8f125476bf22fbf1
aahmadov <abbas@softlinx.com>
2022-12-16 09:39:56 -0500

    push gmail send

--- src/test/java/stepDefinitions/Db_query.java
+++ src/test/java/stepDefinitions/Db_query.java
@@ -13,7 +13,7 @@ public class Db_query {
 		
 		DataBaseUtility.openConnection();
 		//String query = "SELECT JobID,Pages ,FaxNumber,Error FROM acme.sendstatus where JobStatus='send Failed'"; 
-		String query = "select TransmitStationID from acme.sendstatus where FaxUserID ='faxsending' and JobStatus='Send Failed'";
+		String query = "select TransmitStationID from acme1.sendstatus where FaxUserID ='faxsending' and JobStatus='Send Failed'";
 		//String query ="select * from acme.sendstatus where JobID=73";
 		
 		String EmailToFaxQuery ="select JobStatus from auto1.sendstatus where requestOrigin='mail2fax' and FaxNumber='11111111111' order by jobid desc";
