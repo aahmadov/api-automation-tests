607dbdc5ae8852255704316fdb937e4df5be60a2
aahmadov <abbas@softlinx.com>
2023-01-15 19:16:13 -0500

    test case sperated from smoke json file

--- src/test/java/testng/InboundFaxPagesCoverPage.java
+++ src/test/java/testng/InboundFaxPagesCoverPage.java
@@ -15,7 +15,7 @@ import static org.testng.Assert.assertEquals;
 
 public class InboundFaxPagesCoverPage extends TestBase {
 
-    @Test(testName = "Dynamic scenario for fax status and page number validation from inbound", groups = {"smoke"})
+    @Test(testName = "Dynamic scenario for fax status and page number validation from inbound", groups = {"smoke2"})
     public void faxStatusAndPageNumberValidationFromInbound() throws InterruptedException {
         System.out.println("Test case name: " + testName);
         Map<String, String> data = JsonUtils.getDataBasedOnTestCaseName(testName);
