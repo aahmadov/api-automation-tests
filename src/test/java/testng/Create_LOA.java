c7cb488cca0e4138a76fd2ff64768bb08abf5fad
aahmadov <abbas@softlinx.com>
2023-06-22 07:09:19 -0400

    case test

--- src/test/java/testng/Create_LOA.java
+++ src/test/java/testng/Create_LOA.java
@@ -23,7 +23,7 @@ import static org.junit.Assert.assertEquals;
 
 public class Create_LOA extends TestBase {
 
-    @Test(testName = "Creates a PDF letter of Authorization ", groups = {"RegressionIndiTest"})
+    @Test(testName = "Creates a PDF letter of Authorization ", groups = {"Regression"})
     void CreateLOA() throws Exception  {
         System.out.println("Test case name: " + testName);
         Map<String, String> data = JsonUtils.getDataBasedOnTestCaseName(testName);
@@ -50,7 +50,7 @@ public class Create_LOA extends TestBase {
         System.out.println("Page count is: "+count);
     }
 
-    @Test(testName="add_new_number_request/tollFree",groups = {"Regression"})
+    @Test(testName="add_new_number_request/tollFree",groups = {"RegressionIndiTest"})
     void addNewNumberTollFree() throws InterruptedException {
 
         System.out.println("Test case name: " + testName);
