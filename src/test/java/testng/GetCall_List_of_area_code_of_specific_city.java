e09e9c3e4b0393bc42fb5649efaa9ce1c31b9e21
aahmadov <abbas@softlinx.com>
2023-08-10 09:22:44 -0400

    Token2 updated

--- src/test/java/testng/GetCall_List_of_area_code_of_specific_city.java
+++ src/test/java/testng/GetCall_List_of_area_code_of_specific_city.java
@@ -229,7 +229,7 @@ public class GetCall_List_of_area_code_of_specific_city extends TestBase {
         System.out.println(responseGetCall.asPrettyString());
 
     }
-    @Test(testName = "Request to add FaxId with New URL", groups = {"smoke"})
+    @Test(testName = "Request to add FaxId with New URL", groups = {"smoke1"})
     void GetDetailsAboutRequest2() throws Exception {
         System.out.println("Test case name: " + testName);
         Map<String, String> data = JsonUtils.getDataBasedOnTestCaseName(testName);
