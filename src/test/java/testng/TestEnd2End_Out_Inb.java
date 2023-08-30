422695095e4ce99e6ef56d746b60e0895cc9f9bd
aahmadov <abbas@softlinx.com>
2023-08-30 07:42:01 -0400

    regression group name changed

--- src/test/java/testng/TestEnd2End_Out_Inb.java
+++ src/test/java/testng/TestEnd2End_Out_Inb.java
@@ -102,7 +102,7 @@ public class TestEnd2End_Out_Inb extends TestBase {
         }
     }
 
-    @Test(testName = "Dynamic scenario for fax status and page number validation from inbound", groups = {"smoke"})
+    @Test(testName = "Dynamic scenario for fax status and page number validation from inbound", groups = {"smoke1"})
     public void faxStatusAndPageNumberValidationFromInboundCopy() throws InterruptedException {
         System.out.println("Test case name: " + testName);
         Map<String, String> data = JsonUtils.getDataBasedOnTestCaseName(testName);
