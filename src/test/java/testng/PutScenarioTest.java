0d5171e66c2148c63247653f218b7c9a47fd90b8
aahmadov <abbas@softlinx.com>
2023-10-12 08:58:52 -0400

    put sceanrio updated

--- src/test/java/testng/PutScenarioTest.java
+++ src/test/java/testng/PutScenarioTest.java
@@ -19,7 +19,7 @@ import static org.testng.Assert.assertEquals;
 
 public class PutScenarioTest extends TestBase {
 
-    @Test(testName = "Put scenario", groups = {"RegressionFail"})
+    @Test(testName = "Put scenario", groups = {"Regression"})
     public void putScenarioForRegistrySettings() throws InterruptedException {
         System.out.println("Test case name: " + testName);
         Map<String, String> data = JsonUtils.getDataBasedOnTestCaseName(testName);
