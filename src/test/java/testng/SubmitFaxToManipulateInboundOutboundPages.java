11a83c46ccb3d677d75487bd4a0da10ca745765f
aahmadov <abbas@softlinx.com>
2023-11-06 21:09:19 -0500

    Regression is back to previous mode to regression

--- src/test/java/testng/SubmitFaxToManipulateInboundOutboundPages.java
+++ src/test/java/testng/SubmitFaxToManipulateInboundOutboundPages.java
@@ -95,7 +95,7 @@ public class SubmitFaxToManipulateInboundOutboundPages extends TestBase {
             assertTrue(statuses.stream().skip(1).allMatch(status -> status.equals("recvIncomplete")));
         }
     }
-    @Test(testName = "Submit Fax to manipulate outbound&inbound Data", groups = {"RegressionAbb"})
+    @Test(testName = "Submit Fax to manipulate outbound&inbound Data", groups = {"Regression"})
     public void submitFaxToManipulateInboundOutboundPagesCopy() throws Exception {
         System.out.println("Test case name: " + testName);
         Map<String, String> data = JsonUtils.getDataBasedOnTestCaseName(testName);
