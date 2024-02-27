a0e689293e6562ed8bb5c1551ca47772d0d56d34
aahmadov <abbas@softlinx.com>
2024-02-26 23:08:29 -0500

    stftp groups name changed for testing them seperatly

--- src/test/java/testng/SoapInboundFaxPagesCoverPage.java
+++ src/test/java/testng/SoapInboundFaxPagesCoverPage.java
@@ -314,7 +314,7 @@ public class SoapInboundFaxPagesCoverPage extends TestBase {
                // .replace("{contentType}", data.get("contentType"));
     }
 
-    @Test(testName = "SOAP_Have multiple IMGs configured for the scheduler ", groups = {"Regression81test"})
+    @Test(testName = "SOAP_Have multiple IMGs configured for the scheduler ", groups = {"Regression81"})
     public void soapRequestForIMG81() throws InterruptedException, IOException, SQLException {
         System.out.println("Test case name: " + testName);
         Map<String, String> data = JsonUtils.getDataBasedOnTestCaseName81(testName);
