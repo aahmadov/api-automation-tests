a750e546ef6e99199cbeb614d5cdd4466ffdbe85
aahmadov <abbas@softlinx.com>
2024-04-02 22:29:08 -0400

    group name returned back

--- src/test/java/testng/SftpPushTests.java
+++ src/test/java/testng/SftpPushTests.java
@@ -169,7 +169,7 @@ public class SftpPushTests extends TestBase {
 
     }
 
-    @Test(priority = 1, testName = "SFTP - Test Simple Authentication", groups = {"RegressionTest"})
+    @Test(priority = 1, testName = "SFTP - Test Simple Authentication", groups = {"Regression"})
     void testWithSimpleAuthenticationAbbas() throws Exception {
         System.out.println("Test case name: " + testName);
         Map<String, String> data = JsonUtils.getDataBasedOnTestCaseName(testName);
@@ -223,7 +223,7 @@ public class SftpPushTests extends TestBase {
 
     }
 
-    @Test(priority = 3, testName = "SFTP - Test With Certificate No Passphrase", groups = {"RegressionTest"})
+    @Test(priority = 3, testName = "SFTP - Test With Certificate No Passphrase", groups = {"Regression"})
     void testWithCertificateNoPassphraseAbbas() throws Exception {
         System.out.println("Test case name: " + testName);
         Map<String, String> data = JsonUtils.getDataBasedOnTestCaseName(testName);
@@ -267,7 +267,7 @@ public class SftpPushTests extends TestBase {
         assertTrue(SftpUtils.checkFileExist(data.get("username"), data.get("password"), data.get("filePath"), jobId + ".pdf"));
     }
 
-    @Test(priority = 2, testName = "SFTP - Test With Certificate With Passphrase", groups = {"RegressionTest"})
+    @Test(priority = 2, testName = "SFTP - Test With Certificate With Passphrase", groups = {"Regression"})
     void testWithCertificateWithPassphraseAbbas() throws Exception {
         System.out.println("Test case name: " + testName);
         Map<String, String> data = JsonUtils.getDataBasedOnTestCaseName(testName);
