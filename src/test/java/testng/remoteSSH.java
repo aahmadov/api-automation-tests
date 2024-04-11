7162aded6099f94cd0e278fd802caee798cc8e93
aahmadov <abbas@softlinx.com>
2024-04-11 17:54:14 -0400

    2 new case add to regression Purge

---
+++ src/test/java/testng/remoteSSH.java
@@ -0,0 +1,33 @@
+package testng;
+
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+
+public class remoteSSH {
+    public static void main(String[] args) throws JSchException {
+    String host = "10.250.1.84";
+    String user = "Administrator";
+    String password = "5yeDJH4el!#hW";
+
+
+    String commandExe = "C:\\Softlinx\\ReplixServer\\bin\\rpxcleanupd";
+
+
+        JSch jsch = new JSch();
+        Session session = jsch.getSession(user, host, 22);
+        session.setPassword(password);
+        session.setConfig("StrictHostKeyChecking", "no");
+        session.connect();
+
+        ChannelExec channel = (ChannelExec) session.openChannel("exec");
+        channel.setCommand(commandExe);
+        System.out.println();
+        channel.connect();
+
+        channel.disconnect();
+        session.disconnect();
+
+}
+}
