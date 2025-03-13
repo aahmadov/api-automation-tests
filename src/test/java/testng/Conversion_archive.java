5e37363341e97ef519209c739d811e9ba0ea3410
aahmadov <abbas@softlinx.com>
2025-03-13 16:21:16 -0400

    archive conversion directory changed with new one :

--- src/test/java/testng/Conversion_archive.java
+++ src/test/java/testng/Conversion_archive.java
@@ -21,7 +21,7 @@ import static org.junit.Assert.assertTrue;
 
 public class Conversion_archive extends TestBase {
 
-    @Test(priority = 1, testName = "Conversion archive per realm and per realm/user", groups = {"Regression813"})
+    @Test(priority = 1, testName = "Conversion archive per realm and per realm/user", groups = {"Regression81"})
     void conversion_archive_per_realm_and_per_realm_user_81() throws Exception {
         System.out.println("Test case name: " + testName);
         Map<String, String> data = JsonUtils.getDataBasedOnTestCaseName81(testName);
