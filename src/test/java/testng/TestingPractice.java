057241925119b1800d5a3d8bcbab0b9a8f2ccb98
abbas <abbasaydinoglu7@gmail.com>
2025-07-01 20:19:27 -0400

    first push

---
+++ src/test/java/testng/TestingPractice.java
@@ -0,0 +1,19 @@
+package testng;
+
+public class TestingPractice {
+
+    static boolean sound() {
+
+        System.out.println("awoooo");
+        return true;
+    }
+
+    public void Dog () {
+
+    boolean result =TestingPractice.sound();
+        System.out.println(result);
+        System.out.println(result);
+    }
+
+
+}
